8251e48067a933bbbab15c04e3cf8fc60a76fd9f
Farhan <farhankasmani956@gmail.com>
2019-03-21 01:58:15 +0530

    Done

--- src/static_my_proj/js/my.js
+++ src/static_my_proj/js/my.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
+	var x1 = null;
 	$('#send').click(function() {
-		$.ajax({
+		x1 = $.ajax({
 			url: '/send',
+			beforeSend: function() {
+				if (x1 != null) {
+					x1.abort();
+				}
+			},
 			success: function(result) {
 				console.log('Done');
 			}
@@ -9,8 +15,13 @@ $(document).ready(function() {
 	});
 	$('#recieve').click(function() {
 		console.log('Yoyo');
-		$.ajax({
+		x1 = $.ajax({
 			url: '/recieve',
+			beforeSend: function() {
+				if (x1 != null) {
+					x1.abort();
+				}
+			},
 			success: function(result) {
 				console.log('Done');
 			}
